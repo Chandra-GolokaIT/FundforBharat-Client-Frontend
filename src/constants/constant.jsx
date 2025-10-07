ff972f9719e4626a2dd1f8059ecbff16148b6feb
Chandra Sekhar Cherukuru <chandrasekharcherukuruu@gmail.com>
2025-10-07 15:55:53 +0530

    Update BASE_URL to use environment variable

--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,8 +1,1 @@
-//export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://cortez-queenliest-danyelle.ngrok-free.dev";
-//export const ADMIN_URL = "http://56.228.14.216:8080/demo-0.0.1-SNAPSHOT";
-export const ADMIN_URL = "https://cortez-queenliest-danyelle.ngrok-free.dev";
-//export const ADMIN_URL = "http://localhost:8081";
-export const RAZORPAY_KEY = "rzp_test_8b0d1c3f2e5a4f";
-export const RAZORPAY_SECRET = "8b0d1c3f2e5a4f";
-
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://13.60.218.143:8080";
