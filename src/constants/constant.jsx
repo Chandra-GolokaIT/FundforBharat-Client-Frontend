1c17c5ffe98e99e6cb103962e1a6879d3f07b160
Chandra Sekhar Cherukuru <chandrasekharcherukuruu@gmail.com>
2025-10-07 15:56:22 +0530

    Restore RAZORPAY constants

--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,1 +1,4 @@
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://13.60.218.143:8080";
+export const ADMIN_URL = import.meta.env.VITE_API_BASE_URL || "http://13.60.218.143:8080";
+export const RAZORPAY_KEY = "rzp_test_8b0d1c3f2e5a4f";
+export const RAZORPAY_SECRET = "8b0d1c3f2e5a4f";
